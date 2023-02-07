86d929bca0527084d80cd1710f41a6447cd83d8f
Jan Grzybek <jan@amperecomputing.com>
2023-02-07 20:40:26 +0100

    wip

--- ldm/models/diffusion/ddim.py
+++ ldm/models/diffusion/ddim.py
@@ -186,14 +186,18 @@ class DDIMSampler(object):
                       temperature=1., noise_dropout=0., score_corrector=None, corrector_kwargs=None,
                       unconditional_guidance_scale=1., unconditional_conditioning=None,
                       dynamic_threshold=None):
+        print(3*" "+"a")
         b, *_, device = *x.shape, x.device
 
         if unconditional_conditioning is None or unconditional_guidance_scale == 1.:
+            print(3 * " " + "b")
             model_output = self.model.apply_model(x, t, c)
         else:
+            print(3 * " " + "c")
             x_in = torch.cat([x] * 2)
             t_in = torch.cat([t] * 2)
             if isinstance(c, dict):
+                print(3 * " " + "d")
                 assert isinstance(unconditional_conditioning, dict)
                 c_in = dict()
                 for k in c:
@@ -206,23 +210,30 @@ class DDIMSampler(object):
                                 unconditional_conditioning[k],
                                 c[k]])
             elif isinstance(c, list):
+                print(3 * " " + "e")
                 c_in = list()
                 assert isinstance(unconditional_conditioning, list)
                 for i in range(len(c)):
                     c_in.append(torch.cat([unconditional_conditioning[i], c[i]]))
             else:
+                print(3 * " " + "f")
                 c_in = torch.cat([unconditional_conditioning, c])
             model_uncond, model_t = self.model.apply_model(x_in, t_in, c_in).chunk(2)
+            print(3 * " " + "g")
             model_output = model_uncond + unconditional_guidance_scale * (model_t - model_uncond)
+            print(3 * " " + "h")
 
         if self.model.parameterization == "v":
             e_t = self.model.predict_eps_from_z_and_v(x, t, model_output)
+            print(3 * " " + "i")
         else:
             e_t = model_output
+            print(3 * " " + "j")
 
         if score_corrector is not None:
             assert self.model.parameterization == "eps", 'not implemented'
             e_t = score_corrector.modify_score(self.model, e_t, x, t, c, **corrector_kwargs)
+            print(3 * " " + "k")
 
         alphas = self.model.alphas_cumprod if use_original_steps else self.ddim_alphas
         alphas_prev = self.model.alphas_cumprod_prev if use_original_steps else self.ddim_alphas_prev
@@ -233,15 +244,19 @@ class DDIMSampler(object):
         a_prev = torch.full((b, 1, 1, 1), alphas_prev[index], device=device)
         sigma_t = torch.full((b, 1, 1, 1), sigmas[index], device=device)
         sqrt_one_minus_at = torch.full((b, 1, 1, 1), sqrt_one_minus_alphas[index],device=device)
+        print(3 * " " + "l")
 
         # current prediction for x_0
         if self.model.parameterization != "v":
             pred_x0 = (x - sqrt_one_minus_at * e_t) / a_t.sqrt()
+            print(3 * " " + "m")
         else:
             pred_x0 = self.model.predict_start_from_z_and_v(x, t, model_output)
+            print(3 * " " + "n")
 
         if quantize_denoised:
             pred_x0, _, *_ = self.model.first_stage_model.quantize(pred_x0)
+            print(3 * " " + "o")
 
         if dynamic_threshold is not None:
             raise NotImplementedError()
@@ -252,6 +267,7 @@ class DDIMSampler(object):
         if noise_dropout > 0.:
             noise = torch.nn.functional.dropout(noise, p=noise_dropout)
         x_prev = a_prev.sqrt() * pred_x0 + dir_xt + noise
+        print(3 * " " + "p")
         return x_prev, pred_x0
 
     @torch.no_grad()
