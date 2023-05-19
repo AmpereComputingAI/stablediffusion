f366d25f864395a559830a8c63482ff20283eb2a
Jan <jan@amperecomputing.com>
2023-05-19 12:18:57 +0000

    wip

--- run_ampere.sh
+++ run_ampere.sh
@@ -1,2 +1,2 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-python3 $SCRIPT_DIR/scripts/txt2img.py --prompt "a professional photograph of an astronaut riding a horse" --ckpt $SCRIPT_DIR/stable-diffusion-2-1-base/v2-1_512-ema-pruned.ckpt --config $SCRIPT_DIR/configs/stable-diffusion/intel/v2-inference-fp32.yaml --torchscript --precision full --n_iter=8
+python3 $SCRIPT_DIR/scripts/txt2img.py --prompt "a professional photograph of an astronaut riding a horse" --ckpt $SCRIPT_DIR/stable-diffusion-2-1-base/v2-1_512-ema-pruned.ckpt --config $SCRIPT_DIR/configs/stable-diffusion/intel/v2-inference-fp32.yaml --torchscript --precision full --n_iter=1
